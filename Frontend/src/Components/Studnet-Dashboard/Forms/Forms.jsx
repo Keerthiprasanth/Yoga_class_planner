2eb6230933a1eb4ee5a369f7a16679ffebc69c7e
Aneesh0631 <101163524+Aneesh0631@users.noreply.github.com>
2024-05-09 23:46:13 +0530

    can able to view forms

--- Frontend/src/Components/Studnet-Dashboard/Forms/Forms.jsx
+++ Frontend/src/Components/Studnet-Dashboard/Forms/Forms.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-import React, { useState } from 'react';
-import axios from 'axios';
 
 const Forms = () => {
   const initialFormData = {
